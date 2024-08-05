ce92dc6e9cccba4a33593d6dcc14711c26aae6f3
dennis-sketch <dennis.kimani2@student.moringaschool.comgit config --global user.name dennis-sketch>
2024-08-05 13:06:41 +0300

    done

--- lib/get.py
+++ lib/get.py
@@ -8,3 +8,11 @@ response = requests.get(url)
 json_content = json.loads(response.text)
 
 print(json.dumps(json_content, indent=4))
+
+
+url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/locations.json"
+
+response = requests.get(url)
+
+print(response.content)
+
